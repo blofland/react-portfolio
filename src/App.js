9f7f8f4721fd2eccc8ddc34a42f99d9ed7bf03bc
unknown <boedi15@gmail.com>
2021-11-12 14:35:24 -0700

    contact form update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Contact from './components/Contact'
 
 import './App.css'
 
@@ -10,7 +11,7 @@ function App() {
   return (
 <div>
   <Header/>
-
+<Contact/>
     <Projects/>
     <About/> 
      <main>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,3 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+
+function Contact(props){
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+return (
+    <form>
+      <h1>Contact me</h1>
+      <form id="contact-form">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" 
+          onChange={(e) => {
+              setName(e.target.value)}}
+             // onBlur=()=>{}
+              value={name}/>
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email"
+                    onChange={(e) => {
+                        setEmail(e.target.value)}}
+                       // onBlur=()=>{}
+                        value={email}/> 
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" 
+                    onChange={(e) => {
+                        setMessage(e.target.value)}}
+                       // onBlur=()=>{}
+                        value={message}/>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </form>
+    );}
 export default Contact;

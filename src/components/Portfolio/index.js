a6da52839c3524047a4d64cf8089e896fd683d5a
unknown <boedi15@gmail.com>
2021-11-14 16:54:17 -0700

    updated readme

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -11,19 +11,21 @@ import {Container, Row, Col} from "react-bootstrap"
 
 function Portfolio(props) {
     return <section>
+      <h1>Portfolio</h1>
+      <p>Click on image to be taken to live page.</p>
      <div className="container1">
-       <figure><a href="https://goat-meat.herokuapp.com/"><img src={Image1} label="Runbuddy" fluid style={{maxWidth: "100%"}}/></a> <figcaption>Top 5 <a href="https://github.com/browneli003/Top-5"></a>https://github.com/browneli003/Top-5  </figcaption></figure>
-       <figure><a href="https://lernantino.github.io/run-buddy/"><img src={Image2} label="Runbuddy" fluid style={{maxWidth: "100%"}}/></a> <figcaption>RunBuddy https://github.com/blofland/run-buddy </figcaption></figure>
+       <figure><a href="https://goat-meat.herokuapp.com/"><img src={Image1} className="pimage" fluid style={{maxWidth: "100%"}}/></a> <figcaption>Top 5 <a href="https://github.com/browneli003/Top-5">GitHub</a>  </figcaption></figure>
+       <figure><a href="https://lernantino.github.io/run-buddy/"><img src={Image2} className="pimage" fluid style={{maxWidth: "100%"}}/></a> <figcaption>RunBuddy <a href="https://github.com/blofland/run-buddy">Github</a></figcaption></figure>
       </div>
 
       <div className="container1">
-       <figure><a href="https://sdillon215.github.io/Weekender/"><img src={Image3} label="Runbuddy" fluid style={{maxWidth: "100%"}}/></a> <figcaption>Weekender https://github.com/blofland/Weekender</figcaption></figure>
-       <figure><a href="https://blofland.github.io/code-quiz/"><img src={Image4} label="Runbuddy" fluid style={{maxWidth: "100%"}}/></a> <figcaption>JavaQuiz https://github.com/blofland/code-quiz   </figcaption></figure>
+       <figure><a href="https://sdillon215.github.io/Weekender/"><img src={Image3} className="pimage" fluid style={{maxWidth: "100%"}}/></a> <figcaption>Weekender <a href ="https://github.com/blofland/Weekender">GitHub</a></figcaption></figure>
+       <figure><a href="https://blofland.github.io/code-quiz/"><img src={Image4} className="pimage" fluid style={{maxWidth: "100%"}}/></a> <figcaption>JavaQuiz <a href ="https://github.com/blofland/code-quiz">GitHub</a>   </figcaption></figure>
       </div>
 
       <div className="container1">
-       <figure><a href="www.github.com"><img src={Image5} label="Runbuddy" fluid style={{maxWidth: "100%"}}/></a> <figcaption>Testing</figcaption></figure>
-       <figure><a href="www.github.com"><img src={Image6} label="Runbuddy" fluid style={{maxWidth: "100%"}}/></a> <figcaption>Testing</figcaption></figure>
+       <figure><a href="www.github.com"><img src={Image5} className="pimage" fluid style={{maxWidth: "100%"}}/></a> <figcaption>Testing</figcaption></figure>
+       <figure><a href="www.github.com"><img src={Image6} className="pimage" fluid style={{maxWidth: "100%"}}/></a> <figcaption>Testing</figcaption></figure>
       </div>
       
 
